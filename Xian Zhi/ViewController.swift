778045ea38a499a2c7c13e7ab3f8d6678775318a
lanninisgarden <kevinjohn.mcclung@gmail.com>
2020-04-12 16:30:03 -0400

    Original.

--- Xian Zhi/ViewController.swift
+++ Xian Zhi/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var sageAnswer: UIImageView!
+    
+    var answers = [ #imageLiteral(resourceName: "千真万确"), #imageLiteral(resourceName: "也未可知"), #imageLiteral(resourceName: "大错特错")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func askQuestion(_ sender: UIButton) {
+        sageAnswer.image = answers.randomElement()
+    }
+    
 }
 
